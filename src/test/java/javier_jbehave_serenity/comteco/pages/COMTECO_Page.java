3b4cfa0feb37ab963fffe816342c39db97c398ca
Javier Vildozo <javier.vildozo@bolivia.assuresoft.com>
2017-04-18 15:44:42 -0400

    version de alvaro

---
+++ src/test/java/javier_jbehave_serenity/comteco/pages/COMTECO_Page.java
@@ -0,0 +1,82 @@
+package javier_jbehave_serenity.comteco.pages;
+
+import static ch.lambdaj.Lambda.convert;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import ch.lambdaj.function.convert.Converter;
+import net.serenitybdd.core.annotations.findby.By;
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.annotations.findby.How;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+
+
+// https://www.comteco.com.bo/guiatel/guiatelf.php
+
+@DefaultUrl("https://www.comteco.com.bo/guiatel/guiatelf.php")
+public class COMTECO_Page extends PageObject{
+	
+	@FindBy(id="nombre")
+    private WebElementFacade campoNombre;
+	
+	@FindBy(id="nombre")
+    private WebElement campoNombreWE;
+
+	@FindBy(id="ap_pat")
+    private WebElementFacade campoApellido;
+	
+	@FindBy(how = How.NAME, using = "Consultar")
+	private WebElementFacade botonConsultar;
+
+	@FindBy(xpath=".//*[@id='sha']/div[2]/div[4]/small")
+	private WebElementFacade resultado;
+	
+	@FindBy(id = "status")
+	private WebElementFacade dropdown;
+	
+	
+	public void seleccionar_nombre_y_apellido_en_selector() {
+		//Select dropdown = new Select(getDriver().findElement(By.id("status")));
+		dropdown.selectByIndex(1);
+		// dropdown.selectByValue("nombre");
+		//dropdown.selectByVisibleText("Nombre & Apellidos");
+	}
+
+	public void insertNombre(String nombre) {
+		typeInto(campoNombreWE,nombre);
+		typeInto(campoNombre, nombre);
+	}
+
+	public void insertApellido(String apellido) {
+		typeInto(campoApellido, apellido);
+	}
+
+	public void clique_boton_consulta() {
+		botonConsultar.click();
+	}
+
+	public String obtenerNumero() {
+		return resultado.getText();
+	}
+
+	/*
+	 * public Iterable<? super String> obtenerNumero() {
+		return resultado.getText();
+	}
+	 */
+	
+	public WebElementFacade waitFor(WebElementFacade webElement) {
+        return getRenderedView().waitFor(webElement);
+    }
+	
+	  public COMTECO_Page waitForReady() {	       
+	        waitFor(dropdown);
+			return this;
+	  }
+	  	  
+}
--- src/test/java/javier_jbehave_serenity/comteco/pages/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author javier.vildozo
- *
- */
-package javier_jbehave_serenity.comteco.pages;
